b844cbf65a2a86deb057fb5ca7f912976fd4870a
Sujil Kumar K.M <kmsujilkumar@gmail.com>
2022-08-12 00:43:51 +0100

    data load file commands for data load

--- data_load.sql
+++ data_load.sql
@@ -1,6 +1,12 @@
-LOAD DATA LOCAL INFILE 'E:/Dessertation/desertion_dkit_msc_2022_sep/processed_data/output.csv'
+LOAD DATA LOCAL INFILE 'E:/Dessertation/dash_app_skm/processed_data/output.csv'
 INTO TABLE suicides
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\r\n'
 IGNORE 1 LINES
 (country, year, sex, age, suicides, population, sucid_in_hundredk, country_year, yearly_gdp, gdp_per_capita, generation, suicide_perc, internetusers, expenses, employeecompensation, unemployment, physician_price, laborforcetotal, lifeexpectancy, mobilesubscriptions, refugees, selfemployed, electricityacess, continent, country_code, mobilesubscription);
+
+-- cd c:\xampp\mysql\bin
+
+-- mysql.exe -u root --password
+
+-- C:\xampp\mysql\bin\mysql.exe -u root -p
